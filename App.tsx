5d5affc764668a4f46afabcbf9350f9edd5ae3e2
diogo <1230426@isep.ipp.pt>
2024-12-15 23:53:05 +0000

    Tela Jogo Personalizado

--- App.tsx
+++ App.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MainActivity from './src/screens/MainActivity';
-import JogoPersonalizado from './src/screens/JogoPersonalizado'; // Ajuste o caminho
-
-const Stack = createStackNavigator();
+import MainActivity from './src/screens/MainActivity'; // Ajuste o caminho se necessário
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <SafeAreaView style={{ flex: 1 }}>
-                <StatusBar barStyle="light-content" />
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name="MainActivity"
-                        component={MainActivity}
-                        options={{ title: 'Tela Inicial' }}
-                    />
-                    <Stack.Screen
-                        name="JogoPersonalizado"
-                        component={JogoPersonalizado}
-                        options={{ title: 'Jogo Personalizado' }}
-                    />
-                </Stack.Navigator>
-            </SafeAreaView>
-        </NavigationContainer>
+        <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar barStyle="light-content" />
+            <MainActivity />
+        </SafeAreaView>
     );
 };
 
